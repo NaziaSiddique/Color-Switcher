d921edf056d209f946da187da3ba1869c8a0db94
NaziaSiddique <36114589+NaziaSiddique@users.noreply.github.com>
2023-03-03 09:48:04 +0000

    Updated

--- app.js
+++ app.js
@@ -3,9 +3,15 @@ const switchBtn = document.querySelector('.switch');
 const switchPara = document.querySelector('.color');
 
 switchBtn.addEventListener('click', function () {
-  getRandomNum();
-  getRandomNum();
-  getRandomNum();
+  let color1 = getRandomNum(); //red color
+  let color2 = getRandomNum(); //green color
+  let color3 = getRandomNum(); //blue color
+
+  const colorString = `rgb(${color1}, ${color2}, ${color3})`;
+  // console.log(colorString);
+
+  body.style.backgroundColor = colorString;
+  switchPara.innerText = colorString;
 });
 
 function getRandomNum() {
